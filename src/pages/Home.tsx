b4ba850b65917fa9c07006105b3627aacd94e21c
vantorcapital <marco@vantorcapital.com>
2026-06-02 18:46:15 -0400

    Update hero image source

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -57,7 +57,7 @@ export function Home() {
       {/* SECTION 2: HERO IMAGE */}
       <div className="w-full bg-white">
         <img
-          src="/Brickell_Skyline%20copy%20copy%20copy.jpeg"
+          src="/Brickell_Skyline%20copy%20copy%20copy%20copy.jpeg"
           alt={t('home_hero_image_alt')}
           className="w-full h-auto block"
           style={{ maxHeight: '640px', objectFit: 'contain', objectPosition: 'center' }}
