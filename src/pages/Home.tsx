3a34ca08538e5b8b3be91e23cda00e482e786b05
vantorcapital <marco@vantorcapital.com>
2026-06-02 18:51:18 -0400

    Complete task

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -112,7 +112,7 @@ export function Home() {
       {/* SECTION 4: MIDTOWN IMAGE */}
       <div className="w-full bg-white">
         <img
-          src="/Midtown_Miami%20copy.jpeg"
+          src="/Midtown_Miami%20copy%20copy.jpeg"
           alt="Midtown Miami aerial"
           className="w-full h-auto block"
           style={{ maxHeight: '640px', objectFit: 'contain', objectPosition: 'center' }}
